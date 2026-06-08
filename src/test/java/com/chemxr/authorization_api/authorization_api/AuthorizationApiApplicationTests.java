e19bdab5961a98d6970e351eca259afc39aa36d5
Jhosua Daga <xxrandr@gmail.com>
2026-06-07 20:58:35 -0500

    first commit

---
+++ src/test/java/com/chemxr/authorization_api/authorization_api/AuthorizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chemxr.authorization_api.authorization_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
